5a311eab72692fd914d8a8e6c839c0fa7fcf1005
Cesar Jacob Ramos Paz <zcesarrr.tw@gmail.com>
2026-04-08 13:31:37 -0700

    scroll sidebar

--- client/src/components/ModelsSidebar.tsx
+++ client/src/components/ModelsSidebar.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from "./ui/scroll-area";
 import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
 
 type Props = {
@@ -8,16 +9,18 @@ export default function ModelsSidebar({ models }: Props) {
     return (
         <Sidebar variant="inset">
             <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel>Models</SidebarGroupLabel>
-                    <SidebarMenu>
-                        {models.map((item, index) => (
-                            <SidebarMenuItem key={index}>
-                                <SidebarMenuButton>{item}</SidebarMenuButton>
-                            </SidebarMenuItem>
-                        ))}
-                    </SidebarMenu>
-                </SidebarGroup>
+                <ScrollArea className="h-full">
+                    <SidebarGroup>
+                        <SidebarGroupLabel>Models</SidebarGroupLabel>
+                        <SidebarMenu>
+                            {models.map((item, index) => (
+                                <SidebarMenuItem key={index}>
+                                    <SidebarMenuButton>{item}</SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroup>
+                </ScrollArea>
             </SidebarContent>
         </Sidebar>
     );
