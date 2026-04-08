df94fd24f53b413c4592e2e99c52e036ec5c325a
Cesar Jacob Ramos Paz <zcesarrr.tw@gmail.com>
2026-04-08 12:30:06 -0700

    endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
+import multer from "multer";
 
 const PORT = 3000;
+const upload = multer({ dest: "uploads/" });
 
 const app: Express = express();
 
@@ -11,8 +13,24 @@ app.get("/", (req: Request, res: Response) => {
     });
 });
 
-app.post("/model", async (req: Request, res: Response) => {
-    
+app.post("/parse-model", upload.single("file"), async (req: Request, res: Response) => {
+    const file = req.file;
+
+    if (!file) {
+        return res.status(400).json({
+            success: false,
+            error: "No file received",
+        });
+    }
+
+    return res.status(200).json({
+        success: true,
+        data: {
+            originalName: file.originalname,
+            filename: file.fieldname,
+            size: file.size,
+        }
+    });
 });
 
 app.listen(PORT, () => {
