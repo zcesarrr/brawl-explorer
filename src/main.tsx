54ffb796a105cec4c9d308369984e93ea11ad75a
Cesar Jacob Ramos Paz <zcesarrr.tw@gmail.com>
2026-04-07 22:26:26 -0700

    sidebar added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from "./components/theme-provider";
+import { TooltipProvider } from './components/ui/tooltip.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </ThemeProvider>
   </StrictMode>,
 )
