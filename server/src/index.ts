641cd79671a7bebaee01dbc9b0c79cd2f7402243
Cesar Jacob Ramos Paz <zcesarrr.tw@gmail.com>
2026-04-08 12:48:35 -0700

    error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import type { Express, Request, Response } from "express";
+import type { Express, NextFunction, Request, Response } from "express";
 import multer from "multer";
 
 const PORT = 3000;
 const upload = multer({ dest: "uploads/" });
 
 const app: Express = express();
+app.disable('x-powered-by');
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
@@ -30,6 +31,22 @@ app.post("/parse-model", upload.single("file"), async (req: Request, res: Respon
             filename: file.fieldname,
             size: file.size,
         }
+    });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        success: false,
+        error: "Nothing to see here",
+    });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+
+    res.status(500).json({
+        success: false,
+        error: "Something wen wrong",
     });
 });
 
