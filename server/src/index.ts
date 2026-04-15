89f3e7a1e65d3fdbf5da40d85ac16c9f515e9edb
Cesar Jacob Ramos Paz <zcesarrr.tw@gmail.com>
2026-04-14 22:27:32 -0700

    some docker tweaks

--- server/src/index.ts
+++ server/src/index.ts
@@ -53,7 +53,6 @@ app.use(cors({
 }));
 app.use(express.json({ limit: "20mb" }))
 app.disable('x-powered-by');
-app.use(express.json());
 app.use(helmet());
 
 app.get("/models", async (req: Request, res: Response) => {
